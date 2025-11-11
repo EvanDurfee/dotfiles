50c56d0bbe7910c2499e78c6db8509dc22779e95
Evan Durfee <edurfee@pm.me>
2025-11-11 08:15:18 +0100

    Add color-scheme-monitor scripts

---
+++ dot_local/share/color-scheme/scripts/executable_update_kitty_theme.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Script for updating the kitty terminal emulator's color scheme.
+# Updates a symlink to a kitty conf whenever the color-scheme variant changes
+# and reloads kitty.
+
+# Get which kitty to pgrep / pkill (or to skip this script if it isn't installed)
+kitty="$(which kitty 2>/dev/null)"
+if [ $? -ne 0 ]; then
+	printf "Kitty not installed, exiting\n" >&2
+	exit 0
+fi
+os_type="$(grep -E "^ID=" /etc/os-release | sed -E 's/^ID=//')"
+if [ "$os_type" = "ubuntu" ]; then
+	kitty=kitty
+fi
+
+variant="$1"
+
+kitty_config_dir="${XDG_CONFIG_HOME:-$HOME/.config}"/kitty
+light_theme="$kitty_config_dir"/light-theme.conf
+dark_theme="$kitty_config_dir"/dark-theme.conf
+system_theme="$kitty_config_dir"/system-theme.conf
+mkdir -p "$kitty_config_dir"
+
+if [ "$variant" = "--init" ]; then
+	if [ -e "$system_theme" ]; then
+		printf "Color scheme aready set, skipping init\n" >&2
+		exit 0
+	fi
+	variant=1
+fi
+
+case "$variant" in
+	0|2) target_theme="$light_theme";;
+	1) target_theme="$dark_theme";;
+	*) printf "Unrecognized color-scheme variant %s\n" "$variant" >&2; exit 1;;
+esac
+
+printf "Setting kitty color scheme to %s\n" "$(basename "$target_theme")" >&2
+ln -rfs "$target_theme" "$system_theme"
+printf "Signaling kitty to reload\n" >&2
+pkill -USR1 --full --exact --uid "$USER" "$kitty"
