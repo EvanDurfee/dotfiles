7d3b6652ff6a299722ad1612f7d029f19e8e1e87
Evan Durfee <edurfee@pm.me>
2025-11-15 12:27:35 +0100

    Add .ssh/config init

---
+++ run_once_after_01_ssh-init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Initialize freedesktop-color-scheme-monitor symlinks
+cat <<'EOF'
+===================================================
+Initialize .ssh/config
+===================================================
+EOF
+
+# Ensure profile is configured when running on a fresh system
+if [ -z "${DOTFILES_PROFILE_LOADED:-}" ]; then . "${XDG_CONFIG_HOME:-"$HOME"/.config}"/profile/common; fi
+
+if [ -e "$HOME"/.ssh/config ]; then
+	exit 0
+fi
+
+mkdir -p "$HOME"/.ssh
+cat <<'EOF' | tee "$HOME"/.ssh/config >/dev/null
+Host *
+	ServerAliveInterval 60
+	ServerAliveCountMax 240
+# Host mymachine
+# 	RequestTTY yes
+# 	RemoteCommand zsh -l
+EOF
