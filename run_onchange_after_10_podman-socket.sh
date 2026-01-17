580d89a8ea0d5d9b7cf17c5b112af5109e920a5d
Evan Durfee <edurfee@pm.me>
2026-01-17 19:25:24 +0100

    Improve podman socket handling
    Print error message but continue after trying to enable podman socket.

--- run_onchange_after_10_podman-socket.sh
+++ run_onchange_after_10_podman-socket.sh
@@ -7,4 +7,5 @@ cat <<'EOF'
 Enable podman socket
 ===================================================
 EOF
-systemctl --user enable podman.socket
+
+systemctl --user enable podman.socket || echo 'Podman socket enable failed, is it installed?' >&2
